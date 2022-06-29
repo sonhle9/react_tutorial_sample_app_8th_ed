4d6e32a9e5e9ace52f68457f7ea23a1def9e8852
manhnguyenduc122 <manh.nguyenduc@powergateaustralia.com>
2022-06-29 15:11:31 +0700

    graphql

--- src/app/api/index.tsx
+++ src/app/api/index.tsx
@@ -1,18 +1,18 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { GraphQLClient } from 'graphql-request';
 
 let BASE_URL = ''
-console.log('env', process.env)
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://localhost:3003/api'
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://localhost:8080/v1'
 } else {
-  BASE_URL = 'https://railstutorialapi.herokuapp.com/api'
+  BASE_URL = 'https://railstutorialapi.herokuapp.com/v1'
 }
 
-axios.defaults.xsrfCookieName = 'CSRF-TOKEN'
+axios.defaults.xsrfCookieName = 'CSRF-TOKEN';
 
-axios.defaults.xsrfHeaderName = 'X-CSRF-Token'
+axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 const API = axios.create({
   baseURL: BASE_URL,
@@ -21,7 +21,7 @@ const API = axios.create({
     'Accept': 'application/json',
     'x-lang': 'EN'
   },
-})
+});
 
 API.interceptors.request.use(
   function (config: AxiosRequestConfig) {
@@ -37,6 +37,10 @@ API.interceptors.request.use(
     {
       config.headers!.Authorization = `Bearer ${sessionStorage.getItem('token')} ${sessionStorage.getItem('remember_token')}`
     }
+    // const token = localStorage.getItem('token')
+    // if (token) {
+    //   config.headers!.Authorization = `Bearer ${token}`
+    // }
     return config
   },
   function (error) {
@@ -44,13 +48,66 @@ API.interceptors.request.use(
   }
 )
 
+// API.interceptors.response.use((response) => {
+//   return response
+// }, async function (error) {
+//   const originalRequest = error.config;
+
+//   if (error.response.status === 401 && originalRequest.url === `${BASE_URL}/v1/auth/refresh-tokens`) {
+//       history.push('/login');
+//       return Promise.reject(error);
+//   }
+
+//   if (error.response.status === 401 && !originalRequest._retry) {
+
+//       originalRequest._retry = true;
+//       const refreshToken = localStorage.getItem('refreshToken')
+//       const res = await axios.post(`${BASE_URL}/v1/auth/refresh-tokens`,
+//       {
+//         'refreshToken': refreshToken
+//       });
+//     if (res.status === 200) {
+//       localStorage.setItem('token', res.data.refresh.token);
+//       axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+//       return axios(originalRequest);
+//     }
+//   }
+//   return Promise.reject(error);
+// });
+
 API.interceptors.response.use(
   function (response: AxiosResponse) {
-    return response.data
+    return response.data;
   },
   function (error) {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default API
+export default API;
+
+const graphQLClient = new GraphQLClient(
+  `${BASE_URL}/graphql`, {
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'x-lang': 'EN',
+    'Authorization': '',
+  },
+  credentials: 'include',
+});
+
+if (
+  localStorage.getItem('token') && localStorage.getItem('token') !== 'undefined'
+) 
+{
+  graphQLClient.setHeader('Authorization', `Bearer ${localStorage.getItem('token')} ${localStorage.getItem('remember_token')}`)
+}
+else if (
+  sessionStorage.getItem('token') && sessionStorage.getItem('token') !== 'undefined'
+) 
+{
+  graphQLClient.setHeader('Authorization', `Bearer ${sessionStorage.getItem('token')} ${sessionStorage.getItem('remember_token')}`)
+}
+
+export { graphQLClient };
